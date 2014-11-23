795e690d868052c1bd36ff5ba1e894619ccb3540
Gabe Isman <gabe.isman@gmail.com>
2014-11-23 15:53:28 -0600

    Add topojson and mapshaper to package, change mapshaper syntax to match new version, move map to common data folder.

--- scripts/interludes/a-narrow-practice/build.js
+++ scripts/interludes/a-narrow-practice/build.js
@@ -2,20 +2,21 @@
 // here's what you need
 // data/interludes/a-narrow-practice
 //   raw/
-//     maps/
-//       us-10m.json
 //     counties/
 //       fips.csv
 // data/common/raw/
 //   dpic
 //     dpic.csv
+//   maps/
+//     us-10m.json
 //
-// then run `npm install shelljs`
+// then run `npm install` if you haven't already
 // then run `node scripts build.js`
 // enjoy.
 
 require('shelljs/global');
 
+// These should be installed by npm install, this is a sanity check.
 if (!which('topojson')) {
   echo('Sorry, this script requires topojson');
   exit(1);
@@ -26,7 +27,6 @@ if (!which('mapshaper')) {
 }
 
 rm('data/interludes/a-narrow-practice/us.topo.json');
-rm('data/interludes/a-narrow-practice/us.topo-index.json');
 
 exec('node scripts/interludes/a-narrow-practice/dpic-to-json.js');
 exec('node scripts/interludes/a-narrow-practice/isolate-dpic-counties.js');
@@ -34,5 +34,5 @@ exec('node scripts/interludes/a-narrow-practice/clean-county-names.js');
 exec('node scripts/interludes/a-narrow-practice/standardize-dpic-counties.js');
 exec('node scripts/interludes/a-narrow-practice/merge-counts-and-fips.js');
 
-exec('topojson data/interludes/a-narrow-practice/raw/maps/us-10m.json -o data/interludes/a-narrow-practice/raw/maps/us-10m.topo.json -e data/interludes/a-narrow-practice/counties.csv -q 1e4 -p');
-exec('mapshaper data/interludes/a-narrow-practice/raw/maps/us-10m.topo.json -o data/interludes/a-narrow-practice/us.topo.json --visvalingam');
+exec('topojson data/common/raw/maps/us-10m.json -o data/interludes/a-narrow-practice/raw/maps/us-10m.topo.json -e data/interludes/a-narrow-practice/counties.csv -q 1e4 -p');
+exec('mapshaper data/interludes/a-narrow-practice/raw/maps/us-10m.topo.json -o data/interludes/a-narrow-practice/us.topo.json format=topojson -simplify 10% visvalingam');
